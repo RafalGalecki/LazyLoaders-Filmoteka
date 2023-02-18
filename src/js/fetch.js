87f55ba57d848d463dfcf9185a1c91e55835b082
Rafał Gałecki <darthunter@gmail.com>
2023-02-18 14:45:46 +0100

    5 files plus index their imports

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,159 +1,159 @@
-// import axios from 'axios';
-// import Notiflix from 'notiflix';
-// import { renderMovies } from './search-form';
-// //import { loadMovies } from './cards-home';
-// import { renderCardPaginator } from './pagination';
-// import { refreshRendering } from './refreshrendering';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import { renderMovies } from './search-form';
+//import { loadMovies } from './cards-home';
+import { renderCardPaginator } from './pagination';
+import { refreshRendering } from './refreshrendering';
 
-// const warning = document.querySelector('.warning');
-// export const API_KEY = '7e626872ba2c457d969115031d94d6fb';
-// export const BASE_URL = 'https://api.themoviedb.org/3/';
+const warning = document.querySelector('.warning');
+export const API_KEY = '7e626872ba2c457d969115031d94d6fb';
+export const BASE_URL = 'https://api.themoviedb.org/3/';
 
-// export let page = 1;
-// export let movieID;
+export let page = 1;
+export let movieID;
 
-// //fetch for getting movies based on input for searching
-// export const getSearchedMovies = async (searchInput, page = 1) => {
-//   const urlForSearching = ''.concat(
-//     BASE_URL,
-//     'search/movie?api_key=',
-//     API_KEY,
-//     '&query=',
-//     searchInput,
-//     `&page=${page}`
-//   );
+//fetch for getting movies based on input for searching
+export const getSearchedMovies = async (searchInput, page = 1) => {
+  const urlForSearching = ''.concat(
+    BASE_URL,
+    'search/movie?api_key=',
+    API_KEY,
+    '&query=',
+    searchInput,
+    `&page=${page}`
+  );
 
-//   const response = await axios
-//     .get(urlForSearching)
-//     .then(function (response) {
-//       // handle success
-//       //refreshRendering();
+  const response = await axios
+    .get(urlForSearching)
+    .then(function (response) {
+      // handle success
+      //refreshRendering();
 
-//       if (response.data.results.length !== 0) {
-//         warning.textContent = '';
-//         renderMovies(response);
-//         renderCardPaginator(response.data.total_pages, response.data.page);
-//         return response;
+      if (response.data.results.length !== 0) {
+        warning.textContent = '';
+        renderMovies(response);
+        renderCardPaginator(response.data.total_pages, response.data.page);
+        return response;
 
-//       } else {
-//         // if no results found - show warning
-//         warning.textContent = 'Search result not successful. Enter the correct movie name and try again.';
+      } else {
+        // if no results found - show warning
+        warning.textContent = 'Search result not successful. Enter the correct movie name and try again.';
        
-//       }
+      }
 
-//     })
-//     .catch(function (error) {
-//       // handle error
-//       console.log(error);
-//       // Notiflix.Notify.error(
-//       //   'We are sorry, but getting data is impossible in that moment'
-//       // );
-//     });
-//   console.log('Input Response', response);
-//   return response;
-// };
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      // Notiflix.Notify.error(
+      //   'We are sorry, but getting data is impossible in that moment'
+      // );
+    });
+  console.log('Input Response', response);
+  return response;
+};
 
-// //fetch for getting movies for initial website based on daily trending
-// export const getInitialMovies = async () => {
+//fetch for getting movies for initial website based on daily trending
+export const getInitialMovies = async () => {
+  const urlForInitialMovies = ''.concat(
+    BASE_URL,
+    'trending/movie/day?api_key=',
+    API_KEY,
+    `&page=${page}`
+  );
+
+  const response = await axios
+    .get(urlForInitialMovies)
+    .then(function (response) {
+      // handle success
+      return response;
+    })
+    .catch(function (error) {
+      // handle error
+      Notiflix.Notify.warning(
+        'We are sorry, but getting data is impossible in that moment'
+      );
+    });
+
+  return response;
+};
+
+// get movies genres
+export const getGenres = async () => {
+  const urlForGenres = ''.concat(
+    BASE_URL,
+    'genre/movie/list?api_key=',
+    API_KEY
+  );
+
+  const response = await axios
+    .get(urlForGenres)
+    .then(function (response) {
+      // handle success
+      return response.data.genres;
+    })
+    .catch(function (error) {
+      // handle error
+      // handle error
+      Notiflix.Notify.error(
+        'We are sorry, but getting data is impossible in that moment'
+      );
+    });
+
+  return response;
+};
+
+// API configuration: check for images
+// const getConfiguration = async () => {
 //   const urlForInitialMovies = ''.concat(
 //     BASE_URL,
-//     'trending/movie/day?api_key=',
-//     API_KEY,
-//     `&page=${page}`
+//     'configuration?api_key=',
+//     API_KEY
 //   );
 
 //   const response = await axios
 //     .get(urlForInitialMovies)
 //     .then(function (response) {
 //       // handle success
-//       return response;
+//       console.log(response);
 //     })
 //     .catch(function (error) {
 //       // handle error
-//       Notiflix.Notify.warning(
-//         'We are sorry, but getting data is impossible in that moment'
-//       );
+//       console.log(error);
 //     });
 
 //   return response;
 // };
 
-// // get movies genres
-// export const getGenres = async () => {
-//   const urlForGenres = ''.concat(
-//     BASE_URL,
-//     'genre/movie/list?api_key=',
-//     API_KEY
-//   );
+// getConfiguration()
 
-//   const response = await axios
-//     .get(urlForGenres)
-//     .then(function (response) {
-//       // handle success
-//       return response.data.genres;
-//     })
-//     .catch(function (error) {
-//       // handle error
-//       // handle error
-//       Notiflix.Notify.error(
-//         'We are sorry, but getting data is impossible in that moment'
-//       );
-//     });
+//get movie details
+export const getMovieDetails = async movie_id => {
+  const urlForMovieDetails = ''.concat(
+    BASE_URL,
+    `movie/${movie_id}?api_key=`,
+    API_KEY
+  );
 
-//   return response;
-// };
+  const response = await axios
+    .get(urlForMovieDetails)
+    .then(function (response) {
+      // handle success
 
-// // API configuration: check for images
-// // const getConfiguration = async () => {
-// //   const urlForInitialMovies = ''.concat(
-// //     BASE_URL,
-// //     'configuration?api_key=',
-// //     API_KEY
-// //   );
-
-// //   const response = await axios
-// //     .get(urlForInitialMovies)
-// //     .then(function (response) {
-// //       // handle success
-// //       console.log(response);
-// //     })
-// //     .catch(function (error) {
-// //       // handle error
-// //       console.log(error);
-// //     });
-
-// //   return response;
-// // };
-
-// // getConfiguration()
-
-// //get movie details
-// export const getMovieDetails = async movie_id => {
-//   const urlForMovieDetails = ''.concat(
-//     BASE_URL,
-//     `movie/${movie_id}?api_key=`,
-//     API_KEY
-//   );
-
-//   const response = await axios
-//     .get(urlForMovieDetails)
-//     .then(function (response) {
-//       // handle success
-
-//       movieID = response.data.id;
+      movieID = response.data.id;
 
 
-//       return response.data;
-//     })
-//     .catch(function (error) {
-//       // handle error
-//       // handle error
-//       Notiflix.Notify.warning(
-//         'We are sorry, but getting data is impossible in that moment'
-//       );
-//     });
+      return response.data;
+    })
+    .catch(function (error) {
+      // handle error
+      // handle error
+      Notiflix.Notify.warning(
+        'We are sorry, but getting data is impossible in that moment'
+      );
+    });
 
-//   return response;
-// };
+  return response;
+};
 
 
--- src/js/search-form.js
+++ src/js/search-form.js
@@ -1,103 +1,103 @@
-// import debounce from 'lodash.debounce';
-// import { getSearchedMovies, getGenres } from './fetch';
-// import { refreshRendering } from './refreshrendering';
-// import { moviesContainer } from './cards-home';
-// import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
+import { getSearchedMovies, getGenres } from './fetch';
+import { refreshRendering } from './refreshrendering';
+import { moviesContainer } from './cards-home';
+import { Notify } from 'notiflix';
 
-// const DEBOUNCE_DELAY = 3000;
-// export let searchInput;
+const DEBOUNCE_DELAY = 3000;
+export let searchInput;
 
-// const form = document.querySelector('#search-form');
-// const input = document.querySelector('#search-form input');
+const form = document.querySelector('#search-form');
+const input = document.querySelector('#search-form input');
 
-// // export const handleSubmit = debounce(function (e) {
-// //   e.preventDefault();
-// //   searchInput = e.target.value.trim();
-
-// //   refreshRendering();
-// //   getSearchedMovies(searchInput);
-// //   //renderMovies(res.data.results);
-// // }, DEBOUNCE_DELAY);
-
-// export const handleSubmit = function (e) {
+// export const handleSubmit = debounce(function (e) {
 //   e.preventDefault();
-//   searchInput = input.value.trim();
-//   getSearchedMovies(searchInput);
-//   clearInput();
-// };
+//   searchInput = e.target.value.trim();
 
-// form.addEventListener('submit', handleSubmit);
-
-// // ATTENTION!
-// // cards rendering function should be separate to its own js file
-// export function renderMovies(response) {
 //   refreshRendering();
+//   getSearchedMovies(searchInput);
+//   //renderMovies(res.data.results);
+// }, DEBOUNCE_DELAY);
 
-//   //get genres for movies
-//   getGenres().then(el => {
-//     const genres = el;
-//     generateCards(response.data.results, genres);
-//   });
+export const handleSubmit = function (e) {
+  e.preventDefault();
+  searchInput = input.value.trim();
+  getSearchedMovies(searchInput);
+  clearInput();
+};
 
-//   //create set of movie cards
-//   function generateCards(data, genres) {
-//     data.map(movie => {
-//       const genresDesc = getGenresDescription(movie, genres);
-//       createMovieCard(movie, genresDesc);
-//     });
-//   }
+form.addEventListener('submit', handleSubmit);
 
-//   function getGenresDescription(movie, genres) {
-//     return genres.reduce((acc, el) => {
-//       if (movie.genre_ids.includes(el.id)) {
-//         acc.push(el.name);
-//       }
-//       return acc;
-//     }, []);
-//   }
+// ATTENTION!
+// cards rendering function should be separate to its own js file
+export function renderMovies(response) {
+  refreshRendering();
 
-//   //create single movie card element
-//   function createMovieCard(singleMovie, genresDesc) {
-//     let movieWrapper = document.createElement('div');
-//     movieWrapper.classList.add('movie-card');
-//     movieWrapper.setAttribute('id', singleMovie.id);
+  //get genres for movies
+  getGenres().then(el => {
+    const genres = el;
+    generateCards(response.data.results, genres);
+  });
 
-//     //create full url for images
-//     let urlImg = `https://image.tmdb.org/t/p/w300${singleMovie.poster_path}`;
+  //create set of movie cards
+  function generateCards(data, genres) {
+    data.map(movie => {
+      const genresDesc = getGenresDescription(movie, genres);
+      createMovieCard(movie, genresDesc);
+    });
+  }
 
-//     let moviePicture = document.createElement('img');
-//     moviePicture.classList.add('movie-card__img');
-//     moviePicture.setAttribute('src', urlImg);
-//     moviePicture.setAttribute('alt', singleMovie.title);
-//     moviePicture.setAttribute(
-//       'onerror',
-//       "this.src = 'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg'"
-//     );
-//     moviePicture.setAttribute('loading', 'lazy');
+  function getGenresDescription(movie, genres) {
+    return genres.reduce((acc, el) => {
+      if (movie.genre_ids.includes(el.id)) {
+        acc.push(el.name);
+      }
+      return acc;
+    }, []);
+  }
 
-//     let movieTitle = document.createElement('h2');
-//     movieTitle.classList.add('movie-card__title');
-//     movieTitle.textContent = singleMovie.title;
+  //create single movie card element
+  function createMovieCard(singleMovie, genresDesc) {
+    let movieWrapper = document.createElement('div');
+    movieWrapper.classList.add('movie-card');
+    movieWrapper.setAttribute('id', singleMovie.id);
 
-//     let movieInfo = document.createElement('p');
-//     movieInfo.classList.add('movie-card__info');
+    //create full url for images
+    let urlImg = `https://image.tmdb.org/t/p/w300${singleMovie.poster_path}`;
+
+    let moviePicture = document.createElement('img');
+    moviePicture.classList.add('movie-card__img');
+    moviePicture.setAttribute('src', urlImg);
+    moviePicture.setAttribute('alt', singleMovie.title);
+    moviePicture.setAttribute(
+      'onerror',
+      "this.src = 'https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg'"
+    );
+    moviePicture.setAttribute('loading', 'lazy');
+
+    let movieTitle = document.createElement('h2');
+    movieTitle.classList.add('movie-card__title');
+    movieTitle.textContent = singleMovie.title;
+
+    let movieInfo = document.createElement('p');
+    movieInfo.classList.add('movie-card__info');
     
-//     if (genresDesc.length > 3) {
-//       genresDesc = genresDesc.slice(0, 2)
-//       movieInfo.textContent = `${genresDesc.join(', ') + ', Other'} | ${singleMovie.release_date.slice(0, 4)}`;
-//     } else {
-//       movieInfo.textContent = `${genresDesc.join(', ')} | ${singleMovie.release_date.slice(0, 4)}`;
-//     }
+    if (genresDesc.length > 3) {
+      genresDesc = genresDesc.slice(0, 2)
+      movieInfo.textContent = `${genresDesc.join(', ') + ', Other'} | ${singleMovie.release_date.slice(0, 4)}`;
+    } else {
+      movieInfo.textContent = `${genresDesc.join(', ')} | ${singleMovie.release_date.slice(0, 4)}`;
+    }
 
-//     let movieRating = document.createElement('span');
-//     movieRating.classList.add('movie-card__rating');
-//     movieRating.textContent = singleMovie.vote_average.toFixed(1);
+    let movieRating = document.createElement('span');
+    movieRating.classList.add('movie-card__rating');
+    movieRating.textContent = singleMovie.vote_average.toFixed(1);
 
-//     moviesContainer.appendChild(movieWrapper);
-//     movieWrapper.append(moviePicture, movieTitle, movieInfo, movieRating);
-//   }
-// }
+    moviesContainer.appendChild(movieWrapper);
+    movieWrapper.append(moviePicture, movieTitle, movieInfo, movieRating);
+  }
+}
 
-// function clearInput() {
-//   input.value = '';
-// }
+function clearInput() {
+  input.value = '';
+}
