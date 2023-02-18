c0929c6926e6b26f2c62d6b1907c16d6387cf337
Rafał Gałecki <darthunter@gmail.com>
2023-02-18 15:17:32 +0100

    4 files uncommented

--- src/js/genres_test.js
+++ src/js/genres_test.js
@@ -1,38 +1,38 @@
-// const genres = [
-//   { id: 28, name: 'Action' },
-//   { id: 12, name: 'Adventure' },
-//   { id: 16, name: 'Animation' },
-//   { id: 35, name: 'Comedy' },
-//   { id: 80, name: 'Crime' },
-//   { id: 99, name: 'Documentary' },
-//   { id: 18, name: 'Drama' },
-//   { id: 10751, name: 'Family' },
-//   { id: 14, name: 'Fantasy' },
-//   { id: 36, name: 'History' },
-//   { id: 27, name: 'Horror' },
-//   { id: 10402, name: 'Music' },
-//   { id: 9648, name: 'Mystery' },
-//   { id: 10749, name: 'Romance' },
-//   { id: 878, name: 'Science Fiction' },
-//   { id: 10770, name: 'TV Movie' },
-//   { id: 53, name: 'Thriller' },
-//   { id: 10752, name: 'War' },
-//   { id: 37, name: 'Western' },
-// ];
+const genres = [
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 53, name: 'Thriller' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' },
+];
 
-// const genre_ids = [10749, 35];
+const genre_ids = [10749, 35];
 
-// const typeGenres = genres.reduce( (acc,el)=>{
-//     if (genre_ids.includes(el.id))
-//     {acc.push(el.name)}
-//     return acc
-// },[])
+const typeGenres = genres.reduce( (acc,el)=>{
+    if (genre_ids.includes(el.id))
+    {acc.push(el.name)}
+    return acc
+},[])
 
-// console.log(typeGenres);
+console.log(typeGenres);
 
-// const typeGenres2 = genres
-// .filter(el => genre_ids.includes(el.id))
-// .map(el=> el.name)
-// .join("/")
+const typeGenres2 = genres
+.filter(el => genre_ids.includes(el.id))
+.map(el=> el.name)
+.join("/")
 
-// console.log(typeGenres2);
+console.log(typeGenres2);
