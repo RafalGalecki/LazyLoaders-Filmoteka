6af7422a71a2f0a16e6ed772785e98a2b054111e
Rafał Gałecki <darthunter@gmail.com>
2023-02-18 15:23:51 +0100

    up to login.js

--- src/js/students-modal.js
+++ src/js/students-modal.js
@@ -1,52 +1,52 @@
-// const modalLink = document.querySelector('.footer__link');
-// const modalWindow = document.querySelector('.modal');
-// let slides = document.getElementsByClassName('modal__slide');
-// let navlinks = document.getElementsByClassName('modal__navlink');
-// let currentSlide = 0;
+const modalLink = document.querySelector('.footer__link');
+const modalWindow = document.querySelector('.modal');
+let slides = document.getElementsByClassName('modal__slide');
+let navlinks = document.getElementsByClassName('modal__navlink');
+let currentSlide = 0;
 
-// const removingClass = () => {
-//   modalWindow.classList.add('is-hidden');
-// };
+const removingClass = () => {
+  modalWindow.classList.add('is-hidden');
+};
 
-// function changeSlide(n) {
-//   if (n >= slides.length) {
-//     n = 0;
-//   }
-//   if (n < 0) {
-//     n = slides.length - 1;
-//   }
+function changeSlide(n) {
+  if (n >= slides.length) {
+    n = 0;
+  }
+  if (n < 0) {
+    n = slides.length - 1;
+  }
 
-//   slides[currentSlide].classList.toggle('active');
-//   navlinks[currentSlide].classList.toggle('active');
-//   slides[n].classList.toggle('active');
-//   navlinks[n].classList.toggle('active');
+  slides[currentSlide].classList.toggle('active');
+  navlinks[currentSlide].classList.toggle('active');
+  slides[n].classList.toggle('active');
+  navlinks[n].classList.toggle('active');
 
-//   currentSlide = n;
-// }
+  currentSlide = n;
+}
 
-// document.querySelectorAll('.modal__navlink').forEach((bullet, bulletIndex) => {
-//   bullet.addEventListener('click', () => {
-//     if (currentSlide !== bulletIndex) {
-//       changeSlide(bulletIndex);
-//     }
-//   });
-// });
+document.querySelectorAll('.modal__navlink').forEach((bullet, bulletIndex) => {
+  bullet.addEventListener('click', () => {
+    if (currentSlide !== bulletIndex) {
+      changeSlide(bulletIndex);
+    }
+  });
+});
 
-// modalLink.addEventListener('click', () => {
-//   modalWindow.classList.remove('is-hidden');
-// });
+modalLink.addEventListener('click', () => {
+  modalWindow.classList.remove('is-hidden');
+});
 
-// window.addEventListener('click', event => {
-//   if (event.target.classList.contains('modal')) {
-//     removingClass();
-//   }
-// });
+window.addEventListener('click', event => {
+  if (event.target.classList.contains('modal')) {
+    removingClass();
+  }
+});
 
-// window.addEventListener('keydown', event => {
-//   if (event.key === 'Escape') {
-//     removingClass();
-//   }
-// });
+window.addEventListener('keydown', event => {
+  if (event.key === 'Escape') {
+    removingClass();
+  }
+});
 
 
 
