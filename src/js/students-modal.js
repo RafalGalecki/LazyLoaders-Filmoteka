74eb0bd08c143c31fa4c41b91c2851744c9c9bf7
Wiktor Gola <wiktor.gola.95@wp.pl>
2023-02-19 17:52:01 +0100

    Update students-modal.js

--- src/js/students-modal.js
+++ src/js/students-modal.js
@@ -1,7 +1,10 @@
+import {Spinner} from 'spin.js';
+
 const modalLink = document.querySelector('.footer__link');
 const modalWindow = document.querySelector('.modal');
 let slides = document.getElementsByClassName('modal__slide');
 let navlinks = document.getElementsByClassName('modal__navlink');
+let modalSpin = document.querySelector(".modal__spin");
 let slideIndex = 0;
 let timer = null;
 
@@ -44,3 +47,28 @@ window.addEventListener('keydown', event => {
     clearTimeout(timer);
   }
 });
+
+var opts = {
+  lines: 8, // The number of lines to draw
+  length: 10, // The length of each line
+  width: 5, // The line thickness
+  radius: 3, // The radius of the inner circle
+  scale: 1, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  speed: 0.7, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-fade-more', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#ff6b01', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  top: '50%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  zIndex: 2000000000, // The z-index (defaults to 2e9)
+  className: 'spinner', // The CSS class to assign to the spinner
+  position: 'absolute', // Element positioning
+};
+
+
+var spinner = new Spinner(opts).spin();
+modalSpin.appendChild(spinner.el);
