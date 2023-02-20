5284ba6bb46c490ad302492fb1dd348bb8eb3363
Marcin Wrembel <marcin.wrembel@interia.pl>
2023-02-21 00:39:59 +0100

    library changes

--- src/js/library-render.js
+++ src/js/library-render.js
@@ -1,23 +1,39 @@
-import Notiflix from 'notiflix';
 import {
   getWatchedMovies,
   getQueueMovies,
-  renderStorageMovies
+  renderStorageMovies,
 } from './library';
-import { headerLibrary } from './library';
 import { refreshRendering } from './refreshrendering';
+import { watchedMoviesContainer, templCard } from './library';
+
 const libBtnWatched = document.querySelector('.js-btn-watched');
 const libBtnQueue = document.querySelector('.js-btn-queue');
 
-const modalBtnAddWatch = document.querySelector('.btn__addToWatched');
-const modalBtnAddQue = document.querySelector('.btn__addToQue');
-const modalCard = document.querySelector('.modal-card');
-
-headerLibrary.addEventListener('click', libraryEvents);
 //modalCard.addEventListener('click', libraryUpdate);
 
-function libraryEvents(event) {
-  event.preventDefault();
+export function libraryMovies(event) {
+  // event.preventDefault();
+
+  preloader.classList.remove('hidden');
+  refreshRendering();
+  setTimeout(() => {
+// console.log(getWatchedMovies);
+// console.log(getQueueMovies);
+    if (getWatchedMovies[0]) {
+      renderStorageMovies(getWatchedMovies[0]);
+    }
+    if (getQueueMovies[0]) {
+      renderStorageMovies(getQueueMovies[0]);
+    }
+    preloader.classList.add('hidden');
+    libBtnWatched.classList.remove('btn-lib-js-active');
+    libBtnQueue.classList.remove('btn-lib-js-active');
+  }, 500);
+}
+
+export function reviewMovies(event) {
+  // event.preventDefault();
+
   if (event.target.nodeName !== 'BUTTON') {
     return;
   }
@@ -25,39 +41,86 @@ function libraryEvents(event) {
   if (event.target.classList.contains('js-btn-watched')) {
     libBtnWatched.classList.add('btn-lib-js-active');
     libBtnQueue.classList.remove('btn-lib-js-active');
-    preloader.classList.remove("hidden")
+    preloader.classList.remove('hidden');
     refreshRendering();
     setTimeout(() => {
-      renderStorageMovies(getWatchedMovies[0]);
-        preloader.classList.add("hidden")
-      }, 500)
+      // console.log(!!getWatchedMovies[0]);
+      // console.log(getWatchedMovies[0]);
+      if (getWatchedMovies[0]) {
+        renderStorageMovies(getWatchedMovies[0]);
+
+      } else {
+        watchedMoviesContainer.appendChild(templCard);
+      }
+      preloader.classList.add('hidden');
+    }, 500);
   }
 
   if (event.target.classList.contains('js-btn-queue')) {
     libBtnQueue.classList.add('btn-lib-js-active');
     libBtnWatched.classList.remove('btn-lib-js-active');
-    preloader.classList.remove("hidden")
+    preloader.classList.remove('hidden');
     refreshRendering();
     setTimeout(() => {
-      renderStorageMovies(getQueueMovies[0]);
-        preloader.classList.add("hidden")
-      }, 500)
+      // console.log(getWatchedMovies[0]);
+      if (getWatchedMovies[0]) {
+        renderStorageMovies(getQueueMovies[0]);
+      } else {
+        watchedMoviesContainer.appendChild(templCard);
+      }
+      preloader.classList.add('hidden');
+    }, 500);
   }
 }
 
+// function libraryEvents(event) {
+//   console.log(event.currentTarget);
+//   event.preventDefault();
+//   if (event.target.nodeName !== 'BUTTON') {
+//     return;
+//   }
+
+//   if (event.target.classList.contains('js-btn-watched')) {
+//     libBtnWatched.classList.add('btn-lib-js-active');
+//     libBtnQueue.classList.remove('btn-lib-js-active');
+//     preloader.classList.remove('hidden');
+//     refreshRendering();
+//     setTimeout(() => {
+//       console.log(!!getWatchedMovies[0]);
+//       console.log(getWatchedMovies[0]);
+//       if (getWatchedMovies[0]) {
+//         renderStorageMovies(getWatchedMovies[0]);
+//         preloader.classList.add('hidden');
+//       }
+//     }, 500);
+//   }
+
+//   if (event.target.classList.contains('js-btn-queue')) {
+//     libBtnQueue.classList.add('btn-lib-js-active');
+//     libBtnWatched.classList.remove('btn-lib-js-active');
+//     preloader.classList.remove('hidden');
+//     refreshRendering();
+//     setTimeout(() => {
+//       console.log(getWatchedMovies[0]);
+//       if (getWatchedMovies[0]) {
+//         renderStorageMovies(getQueueMovies[0]);
+//         preloader.classList.add('hidden');
+//       }
+//     }, 500);
+//   }
+// }
+
 function libraryUpdate(event) {
   event.preventDefault();
- if (event.target.nodeName !== 'BUTTON') {
-  return;
- }
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
 
- if (event.target.classList.contains('btn__addToWatched')) {
-  renderStorageMovies(getWatchedMovies[0]);
+  if (event.target.classList.contains('btn__addToWatched')) {
+    renderStorageMovies(getWatchedMovies[0]);
+  }
 
- }
-
- if (event.target.classList.contains('btn__addToQue')) {
-  renderStorageMovies(getQueueMovies[0]);
-  
- }
+  if (event.target.classList.contains('btn__addToQue')) {
+    renderStorageMovies(getQueueMovies[0]);
+  }
 }
